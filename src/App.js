26f05bb7274a4cfb5926403298a9f531bda8fb1a
Beatriz Gomez <beatrizgomez92@gmail.com>
2019-03-09 14:14:56 +0100

    Data printed in console about each pokemon from its own url

--- src/App.js
+++ src/App.js
@@ -7,45 +7,70 @@ class App extends Component {
     super(props);
 
     this.state = {
-      results: []
+      results: [],
+      pokeSearch: ''
     };
+
+    this.searchPokemon = this.searchPokemon.bind(this);
   }
 
   componentDidMount() {
     this.getPokemons();
+
   }
 
   getPokemons() {
     requestPokemons()
       .then(data => {
+        const dataResults = data.results.map(item => {
+          let urlPokemon = item.url;
 
-        const dataResults = data.results.map((item, index) => {
-          return { ...item, id: index };
+          const requestUrl = fetch(urlPokemon).then(response => response.json());
+          requestUrl.then(data => {
+            console.log(data)
+          })
         });
 
+        
         this.setState({
           results: dataResults
         });
       });
   }
 
+  searchPokemon(e) {
+    const query = e.currentTarget.value;
+
+    this.setState({
+      pokeSearch: query
+    })
+  }
+
+  // filterPokemons() {
+  //   const filteredResults = this.state.results.filter(item => {
+  //     const pokeName = item.name;
+  //     return this.state.pokeSearch === '' || pokeName.toLocaleLowerCase().includes(this.state.pokeSearch.toLocaleLowerCase())
+  //   })
+
+  //   return filteredResults;
+  // }
+
   render() {
     return (
       <div className="app">
         <header className="app__header">
-         <h1 className="app__title">pokedex</h1>
+          <h1 className="app__title">pokedex</h1>
+          <div className="app__field">
+            <label className="app__field-text" htmlFor="search"></label>
+            <input type="text" className="app__field-search" onKeyUp={this.searchPokemon} />
+          </div>
         </header>
         <main className="app__main">
           <ul className="pokemon__list">
-          {this.state.results.map((item, index) => {
-            return (
-              <li className="pokemon__list-item" key={index}>{item.name}</li>
-            )
-          })}
           </ul>
         </main>
         <footer className="app__footer">
-          Footer
+          <p className="footer__text">Beatriz Gomez | © Adalab 2019</p>
         </footer>
       </div>
     );
--- src/services/PokemonService.js
+++ src/services/PokemonService.js
@@ -1,4 +1,4 @@
-const ENDPOINT = 'https://pokeapi.co/api/v2/pokemon/?limit=25&offset=0';
+const ENDPOINT = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=25';
 
 const requestPokemons = () => fetch(ENDPOINT).then(response => response.json());
 
